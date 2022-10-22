853856d58395183d0184c6f9d6714affb3613bfb
Peter Dippold <peter.dippold@gmail.com>
2022-10-22 16:51:42 +0200

    Functions for model training, inference and computing classification metrics implemented.

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -1,5 +1,8 @@
-from sklearn.metrics import fbeta_score, precision_score, recall_score
+import numpy as np
 
+from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -17,8 +20,15 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    param_grid = {'n_estimators': [500, 700, 1000],
+                  'max_depth': [30, 50, 100],
+                  'min_samples_split': [20, 40, 60]}
 
-    pass
+    base_estimator = RandomForestClassifier(random_state=42)
+
+    clf = GridSearchCV(base_estimator, param_grid, cv=5, scoring='f1', verbose=4)
+    clf.fit(X_train, y_train)
+    return clf.best_estimator_
 
 
 def compute_model_metrics(y, preds):
@@ -48,7 +58,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : sklearn classifier 
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -57,4 +67,41 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    return model.predict(X)
+
+
+def compute_metrics_for_slice(clf, data, X, y, categories):
+    """Compute the metrics of a model for each categorical variable held fixed.
+    
+    Inputs
+    ------
+    clf: sklearn classifier
+        Trained machine learning model.
+    data: pandas.DataFrame
+        Original DataFrame required to select rows according to category values.
+    X : np.array
+        Data used for prediction
+    y : np.array
+        Known labels, binarized.
+    categories: list
+        List of categorical features.
+    """
+    with open('../metrics/slice_output.txt', 'w', encoding='utf-8') as f:
+        for cat in categories:
+            # Compute model performance on slices of cat
+            for val in data[cat].unique():
+                f.writelines(f'Category: {cat}, Value: {val}\n')
+                f.writelines('Metrics:\n')
+                # use column "Unnamed: 0" (copy of data index) to select all 
+                # rows belonging to category "cat".
+                idx = data[data[cat] == val]['Unnamed: 0'].values
+                X_cat = X[np.isin(X[:,0], idx)]
+                y_cat = y[np.isin(X[:,0], idx)]
+                if X_cat.shape[0] == 0:
+                    f.writelines('   No values found, metrics cannot be calculated.\n')
+                else:
+                    pred = inference(clf, X_cat)
+                    precision, recall, fbeta = compute_model_metrics(y_cat, pred)
+                    f.writelines(f'   - Precision: {precision}\n')
+                    f.writelines(f'   - Recall: {recall}\n')
+                    f.writelines(f'   - F1 Score: {fbeta}\n')
--- starter/train_model.py
+++ starter/train_model.py
@@ -1,12 +1,21 @@
 # Script to train machine learning model.
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model
+from ml.model import inference
+from ml.model import compute_metrics_for_slice
+from pandas import read_csv
+from joblib import dump
+from joblib import load
 
 # Add code to load in the data.
+data = read_csv('../data/census_cleaned.csv')
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+# Split data into training and test sets.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -19,10 +28,30 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+# Preprocess the training data with the process_data function.
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label='salary', encoder=encoder,
+    lb = lb, training=False
+)
 
-# Train and save a model.
+# Train and save a model. Binarizer and encoder are required to process
+# new data before feeding it to the classifier.
+clf = train_model(X_train, y_train)
+model = {
+    'encoder': encoder,
+    'lbl_binarizer': lb,
+    'classifier': clf
+}
+
+dump(model, '../model/model.joblib') 
+
+#model = load('../model/model.joblib')
+
+# Compute model performance on slices using test data
+compute_metrics_for_slice(model['classifier'], test, X_test, y_test, cat_features)
